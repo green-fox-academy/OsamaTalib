102d0089cfde23bdb55a376199bd44d3b8bf7158
Osama <osamatalib@yahoo.com>
2018-08-09 22:27:50 +0200

    update

---
+++ week01/day04/src/com/company/DrawDiamond.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawDiamond {
+  public static void main(String[] args) {
+    Scanner Sc = new Scanner(System.in);
+    System.out.println("Please inter a number :");
+    int num = Sc.nextInt();
+    int x = 0;
+    if (num % 2 == 0) {
+      
+      for (int i = 0; i < (num / 2) + 1; i++) {
+        for (int j = 0; j < (num / 2) + 1 - i; j++) {
+          System.out.print(" ");
+        }
+        for (int k = 0; k <= x; k++) {
+          System.out.print("*");
+        }
+        System.out.println();
+        x += 2;
+      }
+
+      int y = (num / 2) + 2;
+
+      for (int i = (num / 2); i > 0; i--) {
+        for (int j = 0; j < ((num / 2) + 2) - i; j++) {
+          System.out.print(" ");
+        }
+        for (int k = 0; k < y; k++) {
+          System.out.print("*");
+        }
+        System.out.println();
+        y -= 2;
+      }
+    }
+  }
+}
---
+++ week02/day01/src/com/company/anagram.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class anagram {
+  public static void main(String[] args) {
+
+  }
+}
