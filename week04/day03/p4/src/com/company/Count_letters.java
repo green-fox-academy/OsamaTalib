185a5cfd9d2f553aa999747f4c5a32d0d205d257
Osama <osamatalib@yahoo.com>
2018-08-24 09:06:28 +0200

    update

--- week04/day03/p4/src/com/company/Count_letters.java
+++ week04/day03/p4/src/com/company/Count_letters.java
@@ -7,27 +7,28 @@ public class Count_letters {
   String word;
 
 
-  public HashMap<String,Integer> check(String word){
-    this.word=word;
-    HashMap<String,Integer> wordMap= new HashMap<>();
-    ArrayList <String> x = new ArrayList<>();
-    String [] word2 = word.split("");
-    for (int z = 0; z <word2.length; z++){
+  public HashMap<String, Integer> check(String word) {
+    this.word = word;
+    HashMap<String, Integer> wordMap = new HashMap<>();
+    ArrayList<String> x = new ArrayList<>();
+    String[] word2 = word.split("");
+    for (int z = 0; z < word2.length; z++) {
       x.add(word2[z]);
     }
 
-    for (int i = 0; i <x.size(); i++){
-      int sum =0;
-      for (int j =i+1 ; j <x.size(); j++){
-        if ( x.get(i).equals(x.get(j))){
-          sum ++;
+    for (int i = 0; i < x.size(); ) {
+      int sum = 0;
+      for (int j = i + 1; j < x.size(); j++) {
+        if (x.get(i).equals(x.get(j))) {
+          sum++;
           x.remove(j);
 
-        }
-      }  sum++;
-      wordMap.put(x.get(i),sum);
+        } else i++;
+      }
+      sum++;
+      wordMap.put(x.get(i), sum);
     }
 
-return wordMap;
+    return wordMap;
   }
 }
--- week04/day03/p4/src/com/company/Count_lettersTest.java
+++ week04/day03/p4/src/com/company/Count_lettersTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.*;
 
 public class Count_lettersTest {
 
-  String word = "Osama";
+  String word = "Osaaa";
 
   @Test
   public void Check (){
     Count_letters obj = new Count_letters();
-   Assert.assertEquals((Integer) 2, obj.check(word).get("a"));
+   Assert.assertEquals((Integer) 3, obj.check(word).get("a"));
   }
 
 }
